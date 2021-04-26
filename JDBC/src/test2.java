e2c42304bdd6cf1e712111a2e41aaef5557e9000
闫兆祥 <963614893@qq.com>
2021-04-26 16:27:47 +0800

    test3

--- JDBC/src/test2.java
+++ JDBC/src/test2.java
@@ -1,5 +1,15 @@
 public class test2 {
     public static void main(String[] args) {
         System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+        System.out.println("21312sadf1");
+
+
     }
 }
