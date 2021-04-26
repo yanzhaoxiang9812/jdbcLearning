b847df4fd847f76a464de66472cc433c9731aaab
闫兆祥 <963614893@qq.com>
2021-04-26 16:36:34 +0800

    test3

--- JDBC/src/test2.java
+++ JDBC/src/test2.java
@@ -1,7 +1,6 @@
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("21312sadf1");
-        System.out.println("git config --global --unset http.proxy");
+        
 
     }
 }
